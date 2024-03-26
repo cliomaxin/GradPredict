0a11e72ce31ac0fd1ff3b2808a7ad3050e4d7375
devmaxwell <developermaxwellkaranja@gmail.com>
2024-03-26 18:17:19 +0300

    modify settings

--- studentdetails/settings.py
+++ studentdetails/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-d&zmgh)l_3zwax976tl83!v(zy8okpv22p4q&wo@5c8zkq3-1p"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app","*", "localhost", "127.0.0.1",]
 
 
 # Application definition
