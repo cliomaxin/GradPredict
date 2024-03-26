e8abd23fe7f3ec4277df46d64b6b5f91ce1ee16e
devmaxwell <developermaxwellkaranja@gmail.com>
2024-03-26 20:29:38 +0300

    txt added

--- studentdetails/settings.py
+++ studentdetails/settings.py
@@ -71,15 +71,15 @@ TEMPLATES = [
 WSGI_APPLICATION = "studentdetails.wsgi.application"
 
 
-# # Database
-# # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 
 # Password validation
