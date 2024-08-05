b89c54e325e9aec6eb3f5ccb964a0708e3523db2
MaxiFitcher <mxkaranja@gmail.com>
2024-08-05 13:46:21 +0300

    trying to connect the static files, index tags modified

--- details/urls.py
+++ details/urls.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+#if Static does not load import these modules
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("view", views.view, name="view"),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+#if Static Files does not load add the above line of code imediately after the bracket for url patterns
